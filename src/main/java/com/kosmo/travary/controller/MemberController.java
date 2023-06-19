f4c0863324e5ff560ebf5cfb40c3babe02e66416
speed158km98 <speed158km98@gmail.com>
2023-06-19 18:59:05 +0900

    현재 진행까지

--- src/main/java/com/kosmo/travary/controller/MemberController.java
+++ src/main/java/com/kosmo/travary/controller/MemberController.java
@@ -1,101 +1,76 @@
 package com.kosmo.travary.controller;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.kosmo.travary.service.CookieInfo;
-import com.kosmo.travary.service.Cookies;
-import com.kosmo.travary.service.JWTokens;
 import com.kosmo.travary.service.impl.member.MemberServiceImpl;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
-@RequestMapping("/kosmo/member")
+@SessionAttributes({"id"})
+@RequestMapping("member")
 public class MemberController {
 
-	@Autowired
-	private MemberServiceImpl memberService;
-	private String tokenName;
-	private String keyName;
-	private String idName;
-
-	
-	public MemberController(CookieInfo cookieInfo) {
-		tokenName = cookieInfo.getTokenName();
-		keyName = cookieInfo.getKeyName();
-		idName = cookieInfo.getIdName();
-	}
-
-	@GetMapping("Login.msp")
+	@GetMapping("Login.do")
 	public String login() {
+		
 		return "member/Login";
 	}
 	
-	@PostMapping("Login.msp")
-	public String loginProcess(@RequestParam Map map,
-														Model model,
-														HttpServletRequest req,
-														HttpServletResponse resp) {
+	@GetMapping("MyPage.do")
+	public String MyPage() {
 		
-		boolean isMember= memberService.isMember(map);
+		return "member/MyPage";
+	}
+	
+	
+	@Autowired
+	MemberServiceImpl memberService;
+	@PostMapping("LoginProcess.do")
+	public String process(@RequestParam Map map,Model model) {
+		
+		//서비스 호출
+		boolean isMember= memberService.isLogin(map);
+		//데이터 저장
 		if(isMember) {
-			//토큰 방식으로 인증처리 구현
-			map.put("keyName", keyName);
-			//만료시간을 30분으로 하고, 자동생성된 비밀키를 secretKey로 하는 토큰 생성
-			int expireMinute = 30;
-			String token = JWTokens.createToken(keyName, map, expireMinute);
-			//자동생성된 secretKey를 로그인한 유저의 key값으로 저장
-			memberService.insertKey(map);
-			Cookies.createCookie(tokenName, token, resp, req, expireMinute);
-			Cookies.createCookie(idName, map.get("id").toString(), resp, req, expireMinute);
-			req.setAttribute("validate"," ");
-			return "Index";
+			model.addAttribute("id",map.get("id"));
 		}
 		else {
 			model.addAttribute("NotMember","아이디와 비번 불일치");
 		}
+		//뷰정보 반환
 		return "member/Login";
 	}
 	
-	@GetMapping("Register.msp")
-	public String register() {
-		return "member/Register";
+	//로그아웃 처리
+	@GetMapping("Logout.do")
+	public String logout(SessionStatus status) {
+		status.setComplete();
+		//뷰정보 반환
+		return "member/Login";
 	}
 	
-	@PostMapping("Register.msp")
-	public String registerProcess(@RequestParam Map map,@RequestParam String[] inter) {
+	//카카오 로그인
+	// 1번 카카오톡에 사용자 코드 받기(jsp의 a태그 href에 경로 있음)
+	@RequestMapping(value = "/kakaoLogin", method = RequestMethod.GET)
+	public ModelAndView kakaoLogin(@RequestParam(value = "code", required = false) String code) throws Throwable {
 
-		String inters = Arrays.toString(inter);
-		map.put("inter", inters.substring(1,inters.length()-1));
-		int affected = memberService.insert(map);
-		return affected==1?"forward:/kosmo/member/Login.msp":"member/Register";
-	}
-	@ExceptionHandler({Exception.class})
-	public String error(Model model,Exception e) {
-		e.printStackTrace();
-		model.addAttribute("error", e.getMessage());
-		return "Index";
-	}
-	
-	@GetMapping("Logout.msp")
-	public String logout(HttpServletRequest request,
-											HttpServletResponse response) {
-		Cookies.removeCookie(idName, request,  response);
-		Cookies.removeCookie(tokenName, request, response);
-		if(request.getAttribute("validate")!=null) {
-			request.removeAttribute("validate");
-		}
-		return "Index";
+		// 1번
+		System.out.println("code:" + code);
+		
+		return null;	
+		// return에 페이지를 해도 되고, 여기서는 코드가 넘어오는지만 확인할거기 때문에 따로 return 값을 두지는 않았음
+
 	}
 }
--- src/main/java/com/kosmo/travary/service/impl/member/MemberServiceImpl.java
+++ src/main/java/com/kosmo/travary/service/impl/member/MemberServiceImpl.java
@@ -1,61 +1,83 @@
 package com.kosmo.travary.service.impl.member;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kosmo.travary.service.DaoService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kosmo.travary.service.BoardDTO;
+import com.kosmo.travary.service.BoardService;
+import com.kosmo.travary.service.ListPagingData;
 
-@Service
-public class MemberServiceImpl implements DaoService {
+import javax.servlet.http.HttpServletRequest;
 
+/*
+ * ID 미 지정시 ID값은 소문자로 시작하는 클래스명
+ * value속성으로 지정도 가능하다
+ */
+@Service("memberService")
+public class MemberServiceImpl implements BoardService<BoardDTO> {
+
+	//매퍼 인터페이스 주입
 	@Autowired
 	private MemberMapper mapper;
 	
 	@Override
-	public int insert(Map map) {
-		int affected = 0;
-		try {
-			affected = mapper.save(map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return affected;
+	public boolean isLogin(Map map) {
+		return mapper.findByIdAndPwd(map);
 	}
 	
-	public int insertKey(Map map) {
-		int affected = 0;
-		try {
-			affected = mapper.saveKey(map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return affected;
-	}
-	
+	/*
+	 *  첫번째 인자 Map : 
+	 *  	검색 안하면 빈 맵, 검색시에는 검색어와 검색 컬럼명이 저장
+	 *  두번째 인자 req :
+	 *  	컨텍스트 패스를 얻기 위함(페이징시 절대경로 지정)
+	 *  세번째 인자 nowPage :
+	 *  	현재 페이지 번호
+	 */
 	@Override
-	public int update(Map map) {
-		return 0;
+	public ListPagingData<BoardDTO> selectList(Map map, HttpServletRequest req, int nowPage) {
+		
+		//글 전체 목록 얻기
+		List records =  mapper.findAll(map);
+		
+		ListPagingData<BoardDTO> listPagingData = ListPagingData.builder()
+																											.records(records)
+																											.build();
+		return listPagingData;
 	}
 
 	@Override
-	public int delete(Map map) {
-		return 0;
-	}
-	@Override
-	public Map selectOne(Map map) {
-		return mapper.findById(map);
-	}
-
-	@Override
-	public List<Map> selectList(Map map) {
+	public BoardDTO selectOne(Map map) {
 		return null;
 	}
-	
-	public boolean isMember(Map map) {
-		return mapper.findByIdAndPwd(map); 
+
+	@Override
+	public int insert(Map map) {
+		return 0;
 	}
+
+	@Override
+	public int update(BoardDTO record) {
+		return 0;
+	}
+
+	@Override
+	public int delete(BoardDTO record) {
+		return 0;
+	}
+	
+	
 
 }
