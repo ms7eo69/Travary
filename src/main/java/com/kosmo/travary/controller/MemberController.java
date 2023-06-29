048a00ac16b1a672f5bef8f89d461d4087b6d44b
ms7eo69 <ms7eo69@gmail.com>
2023-06-29 17:51:25 +0900

    Update MemberController.java

--- src/main/java/com/kosmo/travary/controller/MemberController.java
+++ src/main/java/com/kosmo/travary/controller/MemberController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kosmo.travary.service.CookieInfo;
 import com.kosmo.travary.service.impl.member.KakaoLoginService;
 import com.kosmo.travary.service.impl.member.MemberServiceImpl;
 
