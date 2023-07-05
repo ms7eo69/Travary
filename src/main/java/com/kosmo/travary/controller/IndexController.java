7dbcbf729886634ef98a8d43e673249a0e621ec3
qamoo <pih7807@gmail.com>
2023-07-05 20:00:42 +0900

    index 페이지 url JSTL로 변경

--- src/main/java/com/kosmo/travary/controller/IndexController.java
+++ src/main/java/com/kosmo/travary/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	
-	@GetMapping("/travary")
+	@GetMapping("/")
 	public String index() {
 		return "Index";
 	}
