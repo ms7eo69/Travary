b2728d12d1a191c5e8d0132168bdbef9f6cb4812
speed158km98 <speed158km98@gmail.com>
2023-07-05 19:33:39 +0900

    naverkey.properties 추가

--- src/main/java/com/kosmo/travary/controller/IndexController.java
+++ src/main/java/com/kosmo/travary/controller/IndexController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	
-	@GetMapping("/travary")
+	@RequestMapping("/")
+	public String contextRoot() {
+		return "forward:/travary/Index.msp";
+	}
+	@RequestMapping("/travary/Index.msp")
 	public String index() {
 		return "Index";
-	}
-	@GetMapping("/community.do")
-	public String community() {
-		return "community/List";
 	}
 }
