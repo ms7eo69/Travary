a013bd14691e35fe17becbaf42e3e764a1f75c66
ms7eo69 <ms7eo69@gmail.com>
2023-07-09 01:47:22 +0900

    Oracle Wallet 설정 끝

--- src/main/java/com/kosmo/travary/TravaryApplication.java
+++ src/main/java/com/kosmo/travary/TravaryApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravaryApplication {
 
 	public static void main(String[] args) {
+		
+		//TNS_ADMIN 변수 설정
+		String projectDir = System.getProperty("user.dir");
+        System.setProperty("TNS_ADMIN", projectDir + "/src/main/resources/wallet");
+        
 		SpringApplication.run(TravaryApplication.class, args);
 	}
 
