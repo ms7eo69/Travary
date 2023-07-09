d20aeddce0b70df79189db00608d5b9fd13def83
KIMSeonjun <tjswns531@naver.com>
2023-07-09 17:22:32 +0900

    ..

---
+++ src/main/java/com/kosmo/travary/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.kosmo.travary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
+@Service("admin")
+public class AdminController {
+	
+}
